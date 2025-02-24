31a1c3c0b2cc6fa43f519987143b80d3bc573c8b
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-02-24 19:05:44 -0300

    Adicionando o método login

---
+++ src/services/AuthService.js
@@ -0,0 +1,30 @@
+const User = require("../models/User");
+const UserService = require("../services/UserService");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const AuthService = {
+  async login(email, password) {
+    try {
+      const user = await User.findOne({ where: { email: email } });
+      if (!user) {
+        throw new Error("Usuário não encontrado");
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        throw new Error("Credenciais incorretas");
+      }
+
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+
+      return token;
+    } catch (err) {
+      throw err;
+    }
+  },
+};
+
+module.exports = AuthService;
