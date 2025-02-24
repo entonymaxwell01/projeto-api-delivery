f45d5231fbf55452ddeecb461199183d54b0f156
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-02-24 19:05:20 -0300

    Reestruturando os métodos de autenticação

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,55 +1,36 @@
-const User = require("../models/User");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const AuthService = require("../services/AuthService");
+const UserService = require("../services/UserService");
 
 module.exports = {
   async register(req, res) {
     try {
       const { nome, email, cpf, password } = req.body;
 
-      const userExist = await User.findOne({ where: { email: email } });
-      if (userExist) {
-        return res.status(400).json({ error: "Usuário já cadastrado" });
-      }
-
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      const user = await User.create({
-        nome,
-        email,
-        cpf,
-        password: hashedPassword,
-      });
+      const user = await UserService.register(nome, email, cpf, password);
       return res
         .status(201)
         .json({ user, message: "Usuário cadastrado com sucesso" });
     } catch (err) {
-      console.log(err);
-      return res.status(500).json({ error: "Erro ao cadastrar o usuário" });
+      if (err.message === "Usuário já cadastrado") {
+        return res.status(409).json({ error: err.message });
+      }
+      return res.status(500).json({ error: err.message });
     }
   },
 
   async login(req, res) {
     try {
       const { email, password } = req.body;
-      const user = await User.findOne({ where: { email: email } });
-      if (!user) {
-        return res.status(404).json({ error: "Usuário não encontrado" });
-      }
-
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) {
-        return res.status(400).json({ error: "Senha incorreta" });
-      }
-
-      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-        expiresIn: "1h",
-      });
-
+      const token = await AuthService.login(email, password);
       return res.status(200).json({ token });
     } catch (err) {
-      console.log("Erro no login:", err);
-      return res.status(500).json({ error: "Não foi possível fazer o login}" });
+      if (err.message === "Usuário não encontrado") {
+        return res.status(404).json({ error: err.message });
+      } else if (err.message === "Credenciais incorretas") {
+        return res.status(401).json({ error: err.message });
+      } else {
+        return res.status(500).json({ error: err.message });
+      }
     }
   },
 };
