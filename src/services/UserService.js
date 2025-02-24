265e27cf5595378803390b17faa33906ebbb2a86
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-02-24 19:05:37 -0300

    Adicionando o método register

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,30 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const UserService = {
+  async register(nome, email, cpf, password) {
+    try {
+      const userExist = await User.findOne({ where: { email: email } });
+      if (userExist) {
+        throw new Error("Usuário já cadastrado");
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const user = await User.create({
+        nome,
+        email,
+        cpf,
+        password: hashedPassword,
+      });
+
+      return user;
+    } catch (err) {
+      throw err;
+    }
+  },
+
   async findAll() {
     try {
       const users = await User.findAll({
