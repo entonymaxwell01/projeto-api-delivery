2afecac617001ee2eb553f4b9a99ab698d3a3e9a
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-02-24 21:05:04 -0300

    Definindo middleware de admin nas rotas

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
 const UserController = require("../controllers/UserController");
+const adminMiddleware = require("../middlewares/adminMiddleware");
 
 const router = express.Router();
 
-router.get("/", authMiddleware, UserController.getAllUsers);
-router.get("/:id", authMiddleware, UserController.getUserById);
+router.get("/", authMiddleware, adminMiddleware, UserController.getAllUsers);
+router.get("/:id", authMiddleware, adminMiddleware, UserController.getUserById);
 // router.put("/:id", authMiddleware, UserController.updateUser);
 
 module.exports = router;
