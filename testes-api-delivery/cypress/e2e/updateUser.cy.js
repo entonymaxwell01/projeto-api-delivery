3e0abfa6d5444a5517b628f10b34dfeb843bf16a
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-02-27 19:19:50 -0300

    Realizando testes da rota de update de usuário
    Realizado testes de sucesso ao atualizar dados e um usuário, e de erro ao tentar atualizar um usuário

---
+++ testes-api-delivery/cypress/e2e/updateUser.cy.js
@@ -0,0 +1,121 @@
+import { faker } from "@faker-js/faker";
+
+describe("Testes da rota de update", () => {
+  it("Deve atualizar os dados de um usuário com sucesso", () => {
+    cy.request({
+      method: "PUT",
+      url: "/usuarios/update/13",
+      body: {
+        nome: "Usuário de testes",
+        email: "example10@test.com",
+        cpf: "12891281201",
+      },
+      headers: {
+        Authorization: `Bearer ${Cypress.env("userToken")}`,
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.be.not.null;
+      expect(response.body).to.be.an("object");
+      expect(response.body.message).to.eq("Usuário atualizado com sucesso");
+      expect(response.body.userUpdate.data.nome).to.be.eq("Usuário de testes");
+    });
+  });
+
+  it("Deve retornar erro ao não preencher campos obrigatórios", () => {
+    cy.request({
+      method: "PUT",
+      url: "/usuarios/update/13",
+      body: {
+        nome: "",
+        email: "example10@test.com",
+        cpf: "12891281201",
+      },
+      headers: {
+        Authorization: `Bearer ${Cypress.env("userToken")}`,
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.be.not.null;
+      expect(response.body.error).to.eq("Campos obrigatórios não preenchidos");
+    });
+  });
+
+  it("Deve retornar erro ao não encontrar usuário", () => {
+    cy.request({
+      method: "PUT",
+      url: "/usuarios/update/1000",
+      body: {
+        nome: "Usuário de testes",
+        email: "example10@test.com",
+        cpf: "12891281201",
+      },
+      headers: {
+        Authorization: `Bearer ${Cypress.env("userToken")}`,
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(404);
+      expect(response.body).to.be.not.null;
+      expect(response.body.error).to.eq("Usuário não encontrado");
+    });
+  });
+  it("Deve retornar erro ao não CPF ou EMAIL já estarem sendo utilizados por outro usuário", () => {
+    cy.request({
+      method: "PUT",
+      url: "/usuarios/update/13",
+      body: {
+        nome: "Usuário de testes",
+        email: "example10@test.com",
+        cpf: "12345678910",
+      },
+      headers: {
+        Authorization: `Bearer ${Cypress.env("userToken")}`,
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(409);
+      expect(response.body).to.be.not.null;
+      expect(response.body.error).to.eq(
+        "E-mail ou CPF já estão em uso por outro usuário"
+      );
+    });
+  });
+
+  it("Deve retornar erro ao passar um token inexistente", () => {
+    cy.request({
+      method: "PUT",
+      url: "/usuarios/update/13",
+      body: {
+        nome: "Usuário de testes",
+        email: "example10@test.com",
+        cpf: "12345678910",
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(401);
+      expect(response.body).to.be.not.null;
+      expect(response.body.error).to.eq("Acesso negado");
+    });
+  });
+  it("Deve retornar erro ao passar um token inválido", () => {
+    cy.request({
+      method: "PUT",
+      url: "/usuarios/update/13",
+      body: {
+        nome: "Usuário de testes",
+        email: "example10@test.com",
+        cpf: "12345678910",
+      },
+      headers: {
+        Authorization: `tokeninvalido`,
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.be.not.null;
+      expect(response.body.error).to.eq("Token inválido");
+    });
+  });
+});
