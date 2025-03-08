32244938fc4380de9ec40ec61a09e2d534918cbc
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-03-08 19:30:48 -0300

    Removendo registro do controller de autenticação

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,37 +1,6 @@
 const AuthService = require("../services/AuthService");
-const UserService = require("../services/UserService");
 
 module.exports = {
-  async register(req, res) {
-    try {
-      const { nome, email, cpf, password } = req.body;
-
-      if (!nome || !email || !cpf || !password) {
-        return res
-          .status(400)
-          .json({ error: "Campos obrigatórios não preenchidos" });
-      }
-
-      const user = await UserService.register(nome, email, cpf, password);
-
-      const userResponse = {
-        id: user.id,
-        nome: user.nome,
-        email: user.email,
-        cpf: user.cpf,
-      };
-
-      return res
-        .status(201)
-        .json({ userResponse, message: "Usuário cadastrado com sucesso" });
-    } catch (err) {
-      if (err.message === "Usuário já cadastrado") {
-        return res.status(409).json({ error: err.message });
-      }
-      return res.status(500).json({ error: err.message });
-    }
-  },
-
   async login(req, res) {
     try {
       const { email, password } = req.body;
