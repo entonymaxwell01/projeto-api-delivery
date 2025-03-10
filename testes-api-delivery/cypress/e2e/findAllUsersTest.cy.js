41977997eec0b034563c5c0dd9733a1f2d281d24
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-03-10 11:13:36 -0300

    Adicionando teste de token inválido

--- testes-api-delivery/cypress/e2e/findAllUsersTest.cy.js
+++ testes-api-delivery/cypress/e2e/findAllUsersTest.cy.js
@@ -40,4 +40,21 @@ describe("Teste da rota de retornar todos os usuários", () => {
       expect(response.body.error).to.eq("Acesso negado");
     });
   });
+
+  it("Deve retornar token inválido quando o token for inválido", () => {
+    cy.request({
+      method: "GET",
+      url: "/usuarios/",
+      headers: {
+        authorization:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Miwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzQxNDcyOTczLCJleHAiOjE3NDE0NzY1NzN9.jTBqq6HXAJ9zqu4bm80qq7n6m__KR8lFZ744Zz75bj4",
+      },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.be.not.null;
+      expect(response.body).to.be.an("object");
+      expect(response.body.error).to.eq("Token inválido");
+    });
+  });
 });
