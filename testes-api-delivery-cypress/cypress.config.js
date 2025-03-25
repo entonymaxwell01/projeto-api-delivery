7cda27cd52cf850602fc44316f701121ab081945
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-03-25 08:37:53 -0300

    Configurando geração de relatórios

--- testes-api-delivery-cypress/cypress.config.js
+++ testes-api-delivery-cypress/cypress.config.js
@@ -2,7 +2,18 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+      return config;
+    },
     baseUrl: "http://localhost:3003",
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/report",
+      reportFilename: "[name]/report",
+      overwrite: false,
+      html: true,
+      json: false,
+    },
   },
 });
