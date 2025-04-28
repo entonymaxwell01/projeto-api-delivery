cca3aa43d41d9db64fb8fa37a7d7e035185bc84e
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-04-28 20:04:12 -0300

    Removendo config de report

--- testes-api-delivery-cypress/cypress.config.js
+++ testes-api-delivery-cypress/cypress.config.js
@@ -3,17 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      require("cypress-mochawesome-reporter/plugin")(on);
       return config;
     },
     baseUrl: "http://localhost:3003",
-    reporter: "mochawesome",
-    reporterOptions: {
-      reportDir: "cypress/report",
-      reportFilename: "[name]/report",
-      overwrite: false,
-      html: true,
-      json: true,
-    },
   },
 });
