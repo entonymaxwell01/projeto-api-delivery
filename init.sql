190835a1ee1b0403dd0724be800093d83f27c2dc
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-04-30 09:25:41 -0300

    Criando tabela clientes e inserindo usuário

--- init.sql
+++ init.sql
@@ -9,6 +9,13 @@ CREATE TABLE IF NOT EXISTS usuarios (
     updatedAt TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS clientes (
+    id INTEGER PRIMARY KEY REFERENCES usuarios(id),
+    data_registro TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
 
 DO
 $$
@@ -17,5 +24,12 @@ BEGIN
         INSERT INTO usuarios (nome, email, cpf, password, role)
         VALUES ('Admin', 'test@test.com', '00000000000', '$2b$10$Y/B8njSH2MCCTPhJcAS9MODTYg5XszeejXxTUvdvaz7/0LkIPnGkS', 'admin');
     END IF;
+
+    IF NOT EXISTS (SELECT 1 FROM usuarios WHERE email = 'delete@test.com') THEN
+        INSERT INTO usuarios (nome, email, cpf, password, role)
+        VALUES ('Delete User', 'delete@test.com', '11111111111', '$2b$10$Y/B8njSH2MCCTPhJcAS9MODTYg5XszeejXxTUvdvaz7/0LkIPnGkS', 'cliente');
+    END IF;
 END
+
+
 $$;
